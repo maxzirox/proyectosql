df2dbfd63089a843a43c8609622d6eddbb1d6d8f
Maxi Guzman <32882558+maxzirox@users.noreply.github.com>
2023-12-23 16:50:57 -0300

    funciones agregar, editar y eliminar  añadidad

--- src/clientes/index.js
+++ src/clientes/index.js
@@ -5,7 +5,11 @@ const router = express.Router();
 
 module.exports.ClientesAPI = (app) => {
     router
-        .get('/', ClientesController.obtenerClientes);
+        .get('/', ClientesController.obtenerClientes)
+        .get('/:rut', ClientesController.obtenerPorRut)
+        .post('/', ClientesController.crearCliente)
+        .put('/:rut', ClientesController.editarCliente)
+        .delete('/:rut', ClientesController.removerCliente)
     
     app.use('/api/clientes', router);
 };
--- src/clientes/services.js
+++ src/clientes/services.js
@@ -1,7 +1,7 @@
 const connection = require('../database/index');
 const debug = require('debug')('app:users-controller');
 
-const getAll = () => {
+const obtenerTodos = () => {
     return new Promise((resolve, reject) => {
         connection.query('SELECT * FROM cliente', (error, results) => {
             if (error) {
@@ -14,6 +14,68 @@ const getAll = () => {
     });
 };
 
+const obtenerRut = (rut) => {
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT * FROM cliente WHERE numrut_cli = ${rut}`, (error, results) => {
+            if (error) {
+                console.error('Error al realizar la consulta:', error);
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+};
+
+const agregarCliente = (cliente) => {
+    return new Promise((resolve, reject) => {
+        //console.log('cliente desde services: ', cliente)
+        connection.query('INSERT INTO cliente SET ?', cliente, (error, results) => {
+            if (error) {
+                console.error('Error al realizar la consulta:', error);
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+};
+
+const modificarCliente = (rut, cliente) => {
+    return new Promise((resolve, reject) => {
+        // Crear una cadena de actualización basada en los nuevos datos proporcionados
+        const updateQuery = 'UPDATE cliente SET ? WHERE numrut_cli = ?';
+
+        // Realizar la consulta de actualización
+        connection.query(updateQuery, [cliente, rut], (error, results) => {
+            if (error) {
+                console.error('Error al realizar la consulta de actualización:', error);
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+};
+
+const eliminarCliente = (rut) => {
+    return new Promise((resolve, reject) => {
+        const deleteQuery = 'DELETE FROM cliente WHERE numrut_cli = ?';
+        connection.query(deleteQuery, [rut], (error, results) => {
+            if (error) {
+                console.error('Error al realizar la consulta de eliminación:', error);
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+};
+
 module.exports.ClientesServices = {
-    getAll
+    obtenerTodos,
+    obtenerRut,
+    agregarCliente,
+    modificarCliente,
+    eliminarCliente
 };
